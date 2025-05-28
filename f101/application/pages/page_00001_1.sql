2d5e957b86cbb43f0a98a703b5b109cb3e12ca3d
MUSTAFA <mustafa.mathhar@xpertier.com>
2025-05-28 10:43:33 -0400

    added dark

---
+++ f101/application/pages/page_00001_1.sql
@@ -0,0 +1,26 @@
+prompt --application/pages/page_00001
+begin
+--   Manifest
+--     PAGE: 00001
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2024.11.30'
+,p_release=>'24.2.0'
+,p_default_workspace_id=>1601143731272205
+,p_default_application_id=>101
+,p_default_id_offset=>1812762159896092
+,p_default_owner=>'TEST'
+);
+wwv_flow_imp_page.create_page(
+ p_id=>1
+,p_name=>'Home'
+,p_alias=>'HOME'
+,p_step_title=>'test'
+,p_autocomplete_on_off=>'OFF'
+,p_page_template_options=>'#DEFAULT#'
+,p_protection_level=>'C'
+,p_page_component_map=>'13'
+);
+wwv_flow_imp.component_end;
+end;
+/
